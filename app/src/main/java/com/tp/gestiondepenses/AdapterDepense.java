30e5e6650a9d1a26306446f4a9747b6e9fec1b6c
Madow-s <Madeleinesarr60@gmail.com>
2026-04-22 00:25:45 +0200

    RecyclerView2

--- app/src/main/java/com/tp/gestiondepenses/AdapterDepense.java
+++ app/src/main/java/com/tp/gestiondepenses/AdapterDepense.java
@@ -1,24 +1,45 @@
 package com.tp.gestiondepenses;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.tp.gestiondepenses.conf.entity.Depense;
+
+import java.util.List;
+
 public class AdapterDepense extends RecyclerView.Adapter<ReclyclerViewDepense> {
+
+    Context context;
+    List<Depense> items;
+
+
+    public AdapterDepense (Context context,  List<Depense> items ){
+        this.context= context;
+        this.items= items;
+    }
+
+
     @NonNull
     @Override
     public ReclyclerViewDepense onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ReclyclerViewDepense(LayoutInflater.from(context).inflate(R.layout.item_depense,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReclyclerViewDepense holder, int position) {
+        holder.CategorieView.setText(String.valueOf(items.get(position).getCategorie_id()));
+        holder.MontantView.setText(String.valueOf(items.get(position).getMontant()));
+        holder.MPaiementView.setText(items.get(position).getMoyen_paiement());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
--- app/src/main/java/com/tp/gestiondepenses/ReclyclerViewDepense.java
+++ app/src/main/java/com/tp/gestiondepenses/ReclyclerViewDepense.java
@@ -1,23 +1,23 @@
 package com.tp.gestiondepenses;
 
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ReclyclerViewDepense extends RecyclerView.ViewHolder {
 
-    ImageView imageView;
     TextView CategorieView , MontantView , MPaiementView;
 
 
     public ReclyclerViewDepense(@NonNull View itemView) {
         super(itemView);
-        imageView = itemView.findViewById(R.id.imgdepense);
         CategorieView = itemView.findViewById(R.id.CategorieDepense);
         MontantView = itemView.findViewById(R.id.montantDepense);
-        MPaiementView = itemView.findViewById(R.id.MoyenPaiementDepense)
+        MPaiementView = itemView.findViewById(R.id.MoyenPaiementDepense);
     }
+
+
 }
